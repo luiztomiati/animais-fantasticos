062abf7e637a10182f95cc6f9b6a1e0eb71e95ee
Luiz Tomiati <luizfernandomello.dev@gmail.com>
2025-12-11 18:38:04 -0300

    refatorado o método accordion

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,18 +1,23 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class initAccordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  activeAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
 
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.activeAccordion(item));
     });
+  }
+  init() {
+    if (this.accordionList.length) {
+      this.activeAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }

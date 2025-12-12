314f00b9fcdccced45265737f998f0b4b9d9a00c
Luiz Tomiati <luizfernandomello.dev@gmail.com>
2025-12-11 22:50:23 -0300

    Refatorado o método fetchAnimais

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import TabNav from './modules/tabnav.js';
 
@@ -30,10 +30,9 @@ modal.init();
 
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
-
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
 initAnimacaoScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
